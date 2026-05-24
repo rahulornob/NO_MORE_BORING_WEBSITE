8a946f4cd911bc07121cce94c2e7200433b4439b
Your Name <your.email@example.com>
2026-05-24 16:55:36 +0600

    fix: remove local absolute path for outputFileTracingRoot in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot:
-    "/Users/user/Library/Mobile Documents/com~apple~CloudDocs/NO_MORE_BORING_WEBSITE",
+  /* config options here */
 };
 
 export default nextConfig;
